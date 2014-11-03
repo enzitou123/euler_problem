2286fba61bc7a60327ae067ec891b5399378a7dd
enzo <enzuu@hotmail.com>
2014-11-03 20:08:55 -0300

    added "or"
    now it has only 9 lines

--- euler_e1.py
+++ euler_e1.py
@@ -4,8 +4,6 @@ natural=range(1,1000)
 sum_n=0
 print natural
 for nat in natural:
-    if nat%3==0:
-        sum_n=sum_n+nat
-    elif nat%5==0:
+    if nat%3==0 or nat%5==0:
         sum_n=sum_n+nat
 print sum_n
